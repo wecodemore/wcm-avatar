1e33f8970594792d973fd3cafb5a5dc0ecd2e357
Franz Josef Kaiser <nzzkaiserf@apaadmins-MBP.office.nzz.local>
2015-07-29 14:58:37 +0200

    fix(doc) Fix docblocks, see #2

--- src/Services/AddUserMetaService.php
+++ src/Services/AddUserMetaService.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the WCM Avatar package.
+ *
+ * © Franz Josef Kaiser / wecodemore
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace WCM\User\Avatar\Services;
 
