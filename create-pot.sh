f97881765c7555d08604900bbf103ed80219130e
Franz Josef Kaiser <fjk@strg.at>
2016-10-27 22:49:26 +0200

    Fix: Improve po/mo creation script, see #38

--- create-pot.sh
+++ create-pot.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
-mkdir lang
+#!/usr/bin/env bash
+TARGET="lang"
+LANG="wcmavatar"
+
+echo "\n # Generating Translation #"
+echo " # ---------------------- #"
+
+echo " ==> Generating lang/ dir"
+[[ -d "${TARGET}" ]] || mkdir "${TARGET}"
+
+echo " ==> Generating l18n file"
 xgettext Bootstrap.php src/**/*.php \
 	--language=PHP \
 	--indent \
-	--omit-header \
-	--package-name=wcmavatar \
+	--from-code=UTF-8 \
+	--package-name="${LANG}" \
 	--keyword=__:1 \
 	--keyword=_e:1 \
 	--keyword=_x:1,2c \
@@ -20,8 +29,14 @@ xgettext Bootstrap.php src/**/*.php \
 	--keyword=_n_noop:1,2 \
 	--keyword=_nx_noop:1,2,3c \
 	--keyword=ngettext:1,2 \
-	--default-domain=wcmavatar.po \
+	--default-domain="${LANG}.po" \
 	--sort-by-file \
+	--strict \
 	--width=80 \
 	--output-dir=lang \
+	--msgid-bugs-address=wecodemore@gmail.com \
 	--output=wcmavatar.pot
+
+echo " ==> Testing if the file is valid\n"
+msgfmt -cv "${TARGET}/${LANG}.pot"
+echo "\n"
